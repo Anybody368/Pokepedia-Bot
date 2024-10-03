cb680f87ec340af879adaa2241e46408316000bd
Samuel Chanal <schanal@unistra.fr>
2024-10-03 14:22:53 +0200

    Utilitaire baie plus nécessaire

--- src/Sleep/UtilSleep.java
+++ src/Sleep/UtilSleep.java
@@ -1,5 +1,4 @@
 package Sleep;
-import PokeType;
 
 public class UtilSleep {
     public static String getNomIle(int numIle)
@@ -22,36 +21,5 @@ public class UtilSleep {
             r += " [[Fichier:Miniature Étoile Sleep.png|20px]]";
         }
         return r;
-    }
-
-    public static String getBaie(PokeType type)
-    {
-        String baie = switch (type)
-        {
-            case "Normal" -> "Kika";
-            case "Feu" -> "Mepo";
-            case "Eau" -> "Oran";
-            case "Électrik" -> "Résin";
-            case "Plante" -> "Durin";
-            case "Glace" -> "Fraive";
-            case "Combat" -> "Ceriz";
-            case "Poison" -> "Maron";
-            case "Sol" -> "Figuy";
-            case "Vol" -> "Palma";
-            case "Psy" -> "Mago";
-            case "Insecte" -> "Prine";
-            case "Roche" -> "Sitrus";
-            case "Spectre" -> "Remu";
-            case "Dragon" -> "Nanone";
-            case "Ténèbres" -> "Wiki";
-            case "Acier" -> "Myrte";
-            case "Fée" -> "Pêcha";
-            default -> "";
-        };
-        if(baie.isEmpty()) {
-            System.err.println("ERREUR : Type non valide");
-            System.exit(1);
-        }
-        return baie;
     }
 }
